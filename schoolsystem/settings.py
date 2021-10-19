e6936e6c4281ca47d29895a31356290f44f0df4b
gushimaaceline <celinegushimaa@gmail.com>
2021-10-19 11:45:24 +0300

    Deployement

--- schoolsystem/settings.py
+++ schoolsystem/settings.py
@@ -152,6 +152,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
